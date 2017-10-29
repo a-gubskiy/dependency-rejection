d8ae6cb22d6593a2e98f02b379ca2ebd5a19d7cb
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2017-10-29 10:43:52 +0200

    Update validator C

--- ValidatorC.cs
+++ ValidatorC.cs
@@ -2,10 +2,6 @@ namespace DR
 {
     public class ValidatorC : IValidtor
     {
-        public ValidatorC()
-        {
-        }
-
         public bool Validate(int itemId)
         {
             var item = GlobalRepositorySingletone.Instance.GetItem(itemId);
